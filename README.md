8efa9f6eab83e855fcce385c310e1fb996846093
Leifer Mendez <leifer33@gmail.com>
2023-01-12 21:26:03 +0100

    update

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 **Con esta librería, puedes construir flujos automatizados de conversación de manera agnóstica al proveedor de WhatsApp,** configurar respuestas automatizadas para preguntas frecuentes, recibir y responder mensajes de manera automatizada, y hacer un seguimiento de las interacciones con los clientes.  Además, puedes configurar fácilmente disparadores que te ayudaran a expandir las funcionalidades sin límites. **[Ver más informacion](https://bot-whatsapp.netlify.app/)**
 
 ```shell
-npm i @bot-whatsapp/bot@dev @bot-whatsapp/cli@dev @bot-whatsapp/database@dev @bot-whatsapp/provider@dev
+npm i @bot-whatsapp/bot@dev @bot-whatsapp/cli@dev @bot-whatsapp/database@dev @bot-whatsapp/provider@dev @bot-whatsapp/portal@dev
 ```
 
 
--- app.js
+++ app.js
@@ -1,50 +1,83 @@
-require('dotenv').config()
 const {
   createBot,
   createProvider,
   createFlow,
   addKeyword,
 } = require('@bot-whatsapp/bot')
-require('./server.http')
-const WsProvider = require('@bot-whatsapp/provider/baileys')
-const DBProvider = require('@bot-whatsapp/database/mock')
 
-/**
- * https://api-74xis.strapidemo.com/admin
- * Conexion API Strapi
- * @returns 
- */
-const menuAPI = async () => {
-  setTimeout(() => Promise.resolve([{body:'Pabellon'},{body:'Empanadas'},{body:'Cocido'}]),500) 
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
+/***
+* Simular peticion async http 1.5 segundos
+*/
+const fakeHTTPMenu = async () => {
+  console.log('⚡ Server request!')
+  await delay(1500)
+  console.log('⚡ Server return!')
+  return Promise.resolve([{ body: 'Arepas' }, { body: 'Empanadas' }])
 }
 
-const flujoCash = addKeyword('efectivo').addAnswer('Ok te espero con los billetes')
-const flujosOnline = addKeyword('online').addAnswer(['Te envio el link','https://buy.stripe.com/14kcPBaYQe7M12g8ww'])
+/***
+* Simular peticion async http 0.5 segundos
+*/
+const fakeHTTPPayment = async () => {
+  const link = `https://www.buymeacoffee.com/leifermendez?t=${Date.now()}`
+  console.log('⚡ Server request!')
+  await delay(500)
+  console.log('⚡ Server return!')
+  return Promise.resolve([
+      { body: `Puedes hacer un *pago* en el siguiente link: ${link}` },
+  ])
+}
 
-const flujoPedido = addKeyword(['pedido','pedir'])
-.addAnswer('¿Como quieres pagar en *efectivo* o *online*?', null, null,[flujoCash, flujosOnline])
+const flujoCash = addKeyword('efectivo').addAnswer(
+  'Ok te espero con los billetes'
+)
+const flujosOnline = addKeyword('online').addAnswer(
+  ['Te envio el link'],
+  null,
+  async (_, { flowDynamic }) => {
+      const link = await fakeHTTPPayment()
+      return flowDynamic(link)
+  }
+)
 
-const conversacionPrincipal = addKeyword(['hola','ole','buenas'],)
-.addAnswer('Bienvenido al restaurante *La cuchara de palo 🙌*')
-.addAnswer(`El menu del día es el siguiente`, null, async (_,{flowDynamic}) => {
-  const data = await menuAPI()
-  flowDynamic(data)
-})
-.addAnswer('👉 Si deseas ordenar escribe *pedir*', {delay:1500}, null,[flujoPedido])
+const flujoPedido = addKeyword(['pedido', 'pedir']).addAnswer(
+  '¿Como quieres pagar en *efectivo* o *online*?',
+  null,
+  null,
+  [flujoCash, flujosOnline]
+)
 
-const main = async () => {
-  const adapterDB = new DBProvider()
-  const adapterFlow = createFlow([conversacionPrincipal])
-  const adapterProvider = createProvider(WsProvider)
-
-  createBot(
-      {
-          flow: adapterFlow,
-          provider: adapterProvider,
-          database: adapterDB,
+const conversacionPrincipal = addKeyword(['hola', 'ole', 'buenas'])
+  .addAnswer('Bienvenido al restaurante *La cuchara de palo 🙌*')
+  .addAnswer(
+      `El menu del día es el siguiente`,
+      null,
+      async (_, { flowDynamic }) => {
+          const menu = await fakeHTTPMenu()
+          return flowDynamic(menu)
       }
   )
+  .addAnswer('👉 Si deseas ordenar escribe *pedir*', { delay: 1500 }, null, [
+      flujoPedido,
+  ])
 
+const main = async () => {
+  const adapterDB = new MockAdapter()
+  const adapterFlow = createFlow([conversacionPrincipal])
+  const adapterProvider = createProvider(BaileysProvider)
+
+  createBot({
+      flow: adapterFlow,
+      provider: adapterProvider,
+      database: adapterDB,
+  })
+
+  QRPortalWeb()
 }
 
 main()
